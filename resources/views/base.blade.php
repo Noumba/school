e2b060dc715e424e89c72645ca876fa9c7ad2459
Leonardo daVinci <n.leonardeinstein@gmail.com>
2020-04-07 15:29:39 +0100

    ok thank

---
+++ resources/views/base.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title','Contacts')</title>
+  <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" />
+  <script src="../js/bootstrap.js" type="text/js"></script>
+</head>
+<body>
+  <div class="container">
+    @yield('main')
+  </div>
+  
+</body>
+</html>
---
+++ resources/views/department.blade.php
@@ -0,0 +1,19 @@
+@extends('welcome')
+
+@section('title')
+    Departments
+
+@endsection
+
+@section('content')
+
+
+    <h1>Here is a list of all the present departments</h1>
+
+    <p>Please take your time to access the department of your choice and feel free to contact us</p>
+
+
+@endsection
+
+
+
